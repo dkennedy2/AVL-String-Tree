c92019d9194c37940bf1232ed6d949fba1adc1a7
dkennedy2 <39226432+dkennedy2@users.noreply.github.com>
2018-06-16 14:30:29 -0500

    files

---
+++ QRcode/src/JavaDemo.java
@@ -0,0 +1,21 @@
+import java.util.Random;
+
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class JavaDemo {
+	
+	public void square() {
+	
+		
+		Rectangle square = new Rectangle(0,0,200,200);
+		square.setStroke(Color.WHITE);
+		square.setFill(Color.BLACK);
+		
+	
+	
+
+}}
